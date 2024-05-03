08e47ae2c5663f496ae140775a466a1b1b34865c
Vitaliy Parhomovych <rushvita1991@gmail.com>
2024-05-04 02:34:14 +0300

    123

---
+++ src/Components/App.jsx
@@ -0,0 +1,11 @@
+import css from "./App.module.css";
+
+function App() {
+  return (
+    <div className={css.container}>
+      <h1>Hello world</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/articles-api.js
@@ -0,0 +1,15 @@
+import axios from "axios";
+
+axios.defaults.baseURL = "https://api.themoviedb.org/3/trending/movie/day";
+const options = {
+  headers: {
+    Authorization:
+      "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJjNGVkMGFkYjlkMjYzNzhmNTJjZGIzYTBiYTUxMzZjMiIsInN1YiI6IjY0ZjRkMTc0ZjI5ZDY2MDEwMDA4OWI2MCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ._YqpC9iMCT5Xq1r2H0C24zz4jsE5Fl2A5kJgTpfxnVQ",
+  },
+};
+
+export const fetchArticles = async () => {
+  const response = await axios.get("", options);
+
+  return response.data;
+};
