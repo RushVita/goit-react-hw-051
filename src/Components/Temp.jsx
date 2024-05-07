ab00eacc6b1b451b9b7515f9ccdb3958d909807b
Vitaliy Parhomovych <rushvita1991@gmail.com>
2024-05-07 11:25:56 +0300

    fix

--- src/Components/Temp.jsx
+++ src/Components/Temp.jsx
@@ -2,11 +2,11 @@ import { Route, Routes } from "react-router-dom";
 import { lazy } from "react";
 import { Layout } from "./Layout";
 
-export default function App() {
-  const HomePage = lazy(async () => import("../pages//HomePage"));
-  const MoviesPage = lazy(async () => import("../pages//MoviesPage"));
-  const MovieDetailsPage = lazy(async () => import("../pages//MovieDetailsPage"));
-  const NotFoundPage = lazy(async () => import("../pages//NotFoundPage"));
+export default function Temp() {
+  const HomePage = lazy(async () => import("../pages/HomePage"));
+  const MoviesPage = lazy(async () => import("../pages/MoviesPage"));
+  const MovieDetailsPage = lazy(async () => import("../pages/MovieDetailsPage"));
+  const NotFoundPage = lazy(async () => import("../pages/NotFoundPage"));
   const MovieCast = lazy(async () => import("./MovieCast/MovieCast"));
   const MovieReviews = lazy(async () => import("./MovieReviews/MovieReviews"));
 
--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 import { BrowserRouter } from "react-router-dom";
-import App from "./components/App";
+import Temp from "./Components/Temp";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <Temp />
     </BrowserRouter>
   </React.StrictMode>
 );
